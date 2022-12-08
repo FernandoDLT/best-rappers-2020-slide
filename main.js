bd77c0007ee3888a7ceeadd0b7f7d4c74eb5645c
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-12-08 17:40:48 -0600

    initiate

---
+++ main.js
@@ -0,0 +1,12 @@
+let bestRappers = ['Tupac Shakur', 'Biggie Smalls', 'Ice Cube']
+
+let removed = bestRappers.shift()
+let removedAgain = bestRappers.pop()
+
+bestRappers.unshift()
+bestRappers.push('Dr. Dre')
+
+console.log( bestRappers )
+console.log( removedAgain)
+console.log( removed )
+console.log( bestRappers )
