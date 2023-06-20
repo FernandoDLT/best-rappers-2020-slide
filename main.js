bf2d8b285388fe348a1c090eea48e758cebec9b9
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-06-20 15:11:48 -0600

    Functions practice

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-let bestRappers = ['Tupac Shakur', 'Biggie Smalls', 'Ice Cube']
+let bestRappers = ['Tupac Shakur', 'Biggie Smalls', 'Dr. Dre']
 
 let removed = bestRappers.shift()
 let removedAgain = bestRappers.pop()
 
-bestRappers.unshift()
-bestRappers.push('Dr. Dre')
+// bestRappers.unshift()
+// bestRappers.push('Dr. Dre')
 
+// console.log( bestRappers )
+// console.log( removedAgain)
+// console.log( removed )
+// console.log( bestRappers )
+
+// Using the map() method
+
+let bestRapperOfAllTime = bestRappers.map(x => 'Tupac Shakur')
 console.log( bestRappers )
-console.log( removedAgain)
-console.log( removed )
-console.log( bestRappers )
+
+// bestRapperOfAllTime.unshift('Tupac Shakur')
+// bestRapperOfAllTime.push('Tupac Shakur')
+
+// console.log( bestRapperOfAllTime )
